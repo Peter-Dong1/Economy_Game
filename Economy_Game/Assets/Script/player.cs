0701a1ef5460ad8edf0b9df1451c2a408efee397
Peter-Dong1 <peterdong1951@gmail.com>
2021-12-25 16:57:20 -0500

    Added movable player
    Movable player, but need to add the finished sprite

---
+++ Economy_Game/Assets/Script/player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10.0f;
+
+    [SerializeField] private float horizontal = 0.0f;
+
+    [SerializeField] private float vertical = 0.0f;
+
+    [SerializeField] private float limiter = 0.71f;
+
+    private Rigidbody2D rb;
+
+    
+
+    // Start is called before the first frame update
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if(Input.GetKey(KeyCode.W)) {
+            vertical = moveSpeed;
+        } else if(Input.GetKey(KeyCode.S)) {
+            vertical = -moveSpeed;
+        } else {
+            vertical = 0;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            horizontal = -moveSpeed;
+        } else if(Input.GetKey(KeyCode.D)) {
+            horizontal = moveSpeed;
+        } else {
+            horizontal = 0;
+        }
+
+        if(Input.GetMouseButtonDown(0)) {
+            Debug.Log("Press Primary Button.");
+            
+        }
+        
+    }
+
+    private void FixedUpdate() {
+        if(horizontal != 0 && vertical != 0) {
+            horizontal *= limiter;
+            vertical *= limiter;
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontal, vertical);
+    }
+}
+
